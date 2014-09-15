4cdc3629c24aea738c2407e5efb7ae154c9f1dad
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2014-09-15 15:35:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Ajax Contact Form
+=======================
+
+A Simple Ajax Contact Form developed in PHP with HTML5 Form validation. Has a fallback in jQuery for browsers that do not support HTML5 form validation.
+
+Open the archive [`process.php`](process.php) and set the sender's email in the following variable:
+```php
+$emailTo = '<YOUR_EMAIL_HERE>';
+```
+
+See the online form [here](http://www.pinceladasdaweb.com.br/blog/uploads/ajax-contact-form/)
---
+++ process.php
@@ -0,0 +1,59 @@
+<?php
+// Configure your Subject Prefix and Recipient here
+$subjectPrefix = '[Contact via website]';
+$emailTo       = '<YOUR_EMAIL_HERE>';
+
+$errors = array(); // array to hold validation errors
+$data   = array(); // array to pass back data
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name    = stripslashes(trim($_POST['name']));
+    $email   = stripslashes(trim($_POST['email']));
+    $subject = stripslashes(trim($_POST['subject']));
+    $message = stripslashes(trim($_POST['message']));
+
+
+    if (empty($name)) {
+        $errors['name'] = 'Name is required.';
+    }
+
+    if (!preg_match('/^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/', $email)) {
+        $errors['email'] = 'Email is invalid.';
+    }
+
+    if (empty($subject)) {
+        $errors['subject'] = 'Subject is required.';
+    }
+
+    if (empty($message)) {
+        $errors['message'] = 'Message is required.';
+    }
+
+    // if there are any errors in our errors array, return a success boolean or false
+    if (!empty($errors)) {
+        $data['success'] = false;
+        $data['errors']  = $errors;
+    } else {
+        $subject = "$subjectPrefix $subject";
+        $body    = '
+            <strong>Name: </strong>'.$name.'<br />
+            <strong>Email: </strong>'.$email.'<br />
+            <strong>Message: </strong>'.nl2br($message).'<br />
+        ';
+
+        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
+        $headers .= 'Content-type: text/html; charset=UTF-8' . PHP_EOL;
+        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+
+        mail($emailTo, $subject, $body, $headers);
+
+        $data['success'] = true;
+        $data['message'] = 'Congratulations. Your message has been sent successfully';
+    }
+
+    // return all our data to an AJAX call
+    echo json_encode($data);
+}
