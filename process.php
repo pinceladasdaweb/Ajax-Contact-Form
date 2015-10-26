beced443ca9e91b47d659cb6845b819ac9ed51d9
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-10-26 11:01:55 -0200

    Added new headers to prevent the email is marked as spam

--- process.php
+++ process.php
@@ -41,14 +41,18 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
             <strong>Message: </strong>'.nl2br($message).'<br />
         ';
 
-        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
-        $headers .= 'Content-type: text/html; charset=UTF-8' . PHP_EOL;
-        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers  = "MIME-Version: 1.1" . PHP_EOL;
+        $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
+        $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
+        $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
+        $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
+        $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email>" . PHP_EOL;
         $headers .= "Return-Path: $emailTo" . PHP_EOL;
         $headers .= "Reply-To: $email" . PHP_EOL;
         $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+        $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
 
-        mail($emailTo, $subject, $body, $headers);
+        mail($emailTo, "=?utf-8?B?" . base64_encode($subject) . "?=", $body, $headers);
 
         $data['success'] = true;
         $data['message'] = 'Congratulations. Your message has been sent successfully';
