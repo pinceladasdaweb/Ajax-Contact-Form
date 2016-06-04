78e60b6216cd4703a7c185781fe9179ede584208
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-06-04 13:48:35 -0300

    Add support classes

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'subject' => [
+        'prefix' => '[Contact Form]'
+    ],
+    'emails' => [
+        'to'   => '',
+        'from' => ''
+    ],
+    'messages' => [
+        'error'   => 'There was an error sending, please try again later.',
+        'success' => 'Your message has been sent successfully.',
+        'validation' => [
+            'emptyname'    => 'Name is required.',
+            'emptyemail'   => 'Email is invalid.',
+            'emptysubject' => 'Subject is required.',
+            'emptymessage' => 'Message is required.'
+        ]
+    ],
+    'fields' => [
+        'name'     => 'Name',
+        'email'    => 'Email',
+        'phone'    => 'Phone',
+        'subject'  => 'Subject',
+        'message'  => 'Message',
+        'btn-send' => 'Send'
+    ]
+];
