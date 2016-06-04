325c5fece5021b259dbe77972ec584e03a54a7c1
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-06-04 17:44:41 -0300

    Removed jQuery dependencie

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -1,72 +1,195 @@
-/*
---------------------------------
-Ajax Contact Form
---------------------------------
-+ https://github.com/pinceladasdaweb/Ajax-Contact-Form
-+ A Simple Ajax Contact Form developed in PHP with HTML5 Form validation.
-+ Has a fallback in jQuery for browsers that do not support HTML5 form validation.
-+ version 1.0.1
-+ Copyright 2014 Pedro Rogerio
-+ Licensed under the MIT license
-+ https://github.com/pinceladasdaweb/Ajax-Contact-Form
-*/
+(function (root, factory) {
+    "use strict";
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.ContactForm = factory();
+    }
+}(this, function () {
+    "use strict";
 
-(function ($, window, document, undefined) {
-    'use strict';
+    var ContactForm = function (form, options) {
+        if (!this || !(this instanceof ContactForm)) {
+            return new ContactForm(form, options);
+        }
 
-    var $form = $('#contact-form');
+        if (!form || !options) {
+            return;
+        }
 
-    $form.submit(function (e) {
-        // remove the error class
-        $('.form-group').removeClass('has-error');
-        $('.help-block').remove();
+        this.form     = form.constructor.name === "Node" ? form : document.querySelector(form);
+        this.endpoint = options.endpoint;
 
-        // get the form data
-        var formData = {
-            'name' : $('input[name="form-name"]').val(),
-            'email' : $('input[name="form-email"]').val(),
-            'subject' : $('input[name="form-subject"]').val(),
-            'message' : $('textarea[name="form-message"]').val()
-        };
+        this.send();
+    };
 
-        // process the form
-        $.ajax({
-            type : 'POST',
-            url  : 'process.php',
-            data : formData,
-            dataType : 'json',
-            encode : true
-        }).done(function (data) {
-            // handle errors
+    ContactForm.prototype = {
+        hasClass: function (el, name) {
+            return new RegExp('(\\s|^)' + name + '(\\s|$)').test(el.className);
+        },
+        addClass: function (el, name) {
+            if (!this.hasClass(el, name)) {
+                el.className += (el.className ? ' ' : '') + name;
+            }
+        },
+        removeClass: function (el, name) {
+            if (this.hasClass(el, name)) {
+                el.className = el.className.replace(new RegExp('(\\s|^)' + name + '(\\s|$)'), ' ').replace(/^\s+|\s+$/g, '');
+            }
+        },
+        each: function (collection, iterator) {
+            var i, len;
+
+            for (i = 0, len = collection.length; i < len; i += 1) {
+                iterator(collection[i], i, collection);
+            }
+        },
+        template: function (string, data) {
+            var piece;
+
+            for (piece in data) {
+                if (Object.prototype.hasOwnProperty.call(data, piece)) {
+                    string = string.replace(new RegExp('{' + piece + '}', 'g'), data[piece]);
+                }
+            }
+
+            return string;
+        },
+        empty: function (el) {
+            while (el.firstChild) {
+                el.removeChild(el.firstChild);
+            }
+        },
+        removeElementsByClass: function (className) {
+            var elements = document.getElementsByClassName(className);
+
+            while(elements.length > 0){
+                elements[0].parentNode.removeChild(elements[0]);
+            }
+        },
+        post: function (path, data, callback) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.open('POST', path, true);
+            xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            xhttp.setRequestHeader('Content-length', data.length);
+            xhttp.setRequestHeader('Connection', 'close');
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status >= 200 && this.status < 300) {
+                        var response = '';
+                        try {
+                            response = JSON.parse(this.responseText);
+                        } catch (err) {
+                            response = this.responseText;
+                        }
+                        callback.call(this, response);
+                    } else {
+                        throw new Error(this.status + " - " + this.statusText);
+                    }
+                }
+            };
+            xhttp.send(data);
+            xhttp = null;
+        },
+        param: function (data) {
+            var params = typeof data === 'string' ? data : Object.keys(data).map(
+                function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]); }
+            ).join('&');
+
+            return params;
+        },
+        send: function () {
+            this.form.addEventListener('submit', function (e) {
+                e.preventDefault();
+
+                var elements = document.querySelectorAll('.form-control'),
+                    formData;
+
+                this.each(elements, function (el, i) {
+                    if (this.hasClass(el.parentNode, 'has-error')) {
+                        this.removeClass(el.parentNode, 'has-error');
+                        this.removeElementsByClass('help-block');
+                    }
+                }.bind(this));
+
+                formData = {
+                    'name'    : document.querySelector('input[name="form-name"]').value,
+                    'email'   : document.querySelector('input[name="form-email"]').value,
+                    'subject' : document.querySelector('input[name="form-subject"]').value,
+                    'message' : document.querySelector('textarea[name="form-message"]').value
+                };
+
+                this.post(this.endpoint, this.param(formData), this.feedback.bind(this));
+            }.bind(this), false);
+        },
+        feedback: function (data) {
             if (!data.success) {
                 if (data.errors.name) {
-                    $('#name-field').addClass('has-error');
-                    $('#name-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.name + '</span>');
+                    var name = document.querySelector('input[name="form-name"]').parentNode,
+                        error;
+
+                    this.addClass(name, 'has-error');
+                    error = this.template(
+                        '<span class="help-block">{report}</span>', {
+                        report: data.errors.name
+                    });
+
+                    name.insertAdjacentHTML('beforeend', error);
                 }
 
                 if (data.errors.email) {
-                    $('#email-field').addClass('has-error');
-                    $('#email-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.email + '</span>');
+                    var email = document.querySelector('input[name="form-email"]').parentNode,
+                        error;
+
+                    this.addClass(email, 'has-error');
+                    error = this.template(
+                        '<span class="help-block">{report}</span>', {
+                        report: data.errors.email
+                    });
+
+                    email.insertAdjacentHTML('beforeend', error);
                 }
 
                 if (data.errors.subject) {
-                    $('#subject-field').addClass('has-error');
-                    $('#subject-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.subject + '</span>');
+                    var subject = document.querySelector('input[name="form-subject"]').parentNode,
+                        error;
+
+                    this.addClass(subject, 'has-error');
+                    error = this.template(
+                        '<span class="help-block">{report}</span>', {
+                        report: data.errors.subject
+                    });
+
+                    subject.insertAdjacentHTML('beforeend', error);
                 }
 
                 if (data.errors.message) {
-                    $('#message-field').addClass('has-error');
-                    $('#message-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.message + '</span>');
+                    var message = document.querySelector('textarea[name="form-message"]').parentNode,
+                        error;
+
+                    this.addClass(message, 'has-error');
+                    error = this.template(
+                        '<span class="help-block">{report}</span>', {
+                        report: data.errors.message
+                    });
+
+                    message.insertAdjacentHTML('beforeend', error);  
                 }
             } else {
-                // display success message
-                $form.html('<div class="alert alert-success">' + data.message + '</div>');
-            }
-        }).fail(function (data) {
-            // for debug
-            console.log(data)
-        });
+                var success = this.template(
+                    '<div class="alert alert-success">{report}</div>', {
+                    report: data.message
+                });
 
-        e.preventDefault();
-    });
-}(jQuery, window, document));
+                this.empty(this.form);
+                this.form.insertAdjacentHTML('beforeend', success);
+            }
+        }
+    }
+
+    return ContactForm;
+}));
